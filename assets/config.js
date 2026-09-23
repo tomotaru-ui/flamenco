8c834ca055e10d5009423365ca3a1635acabe74a
tomotaru-ui <tomotaru@gmail.com>
2026-09-23 17:45:51 +0900

    Wire up deployed Apps Script web app URL

--- assets/config.js
+++ assets/config.js
@@ -3,4 +3,4 @@
 // 発行された exec URL をここに貼り付けてください。
 // (README.md の「デプロイ手順」を参照)
 // ==============================================================
-const GAS_URL = "https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT_ID/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbw2hCvfidV1Z6ErdKD7uo8UmycQWWcva-YBkK1r7bOpOac4QMA1Vdri0ja_v3IIknTC/exec";
